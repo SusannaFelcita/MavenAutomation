53525c32a75e6db22c00d8967c7c681f7f1dbcb4
Susanna <susannafelcita@gmail.com>
2023-10-06 12:29:26 +1100

    commit actions

--- src/test/java/testcases/SampleTest.java
+++ src/test/java/testcases/SampleTest.java
@@ -2,13 +2,15 @@ package testcases;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
 import util.Reusable;
 
 import static util.Reusable.*;
 
 
 public class SampleTest {
-    public static void main(String[] args) {
+    @Test
+    public static void main() {
 
         WebDriver driver= openBrowser("chrome");
         openPage("https://google.com");
